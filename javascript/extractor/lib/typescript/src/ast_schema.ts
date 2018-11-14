3d680da4211785d0c330aa407fc8b1ee8213a9a7
Asger F <asger@semmle.com>
2018-11-14 13:10:49 +0000

    TS: whitelist properties of functions

--- javascript/extractor/lib/typescript/src/ast_schema.ts
+++ javascript/extractor/lib/typescript/src/ast_schema.ts
@@ -19,6 +19,29 @@ let baseSchema = [
 
 astNodeSchemas[ts.SyntaxKind.Identifier] = ["text", "escapedText"];
 
+let functionSchema = [
+  "flags",
+  "modifiers",
+  "decorators",
+  "name",
+  "type",
+  "parameters",
+  "typeParameters",
+  "body",
+  "asteriskToken",
+];
+
+astNodeSchemas[ts.SyntaxKind.CallSignature] = functionSchema;
+astNodeSchemas[ts.SyntaxKind.ConstructSignature] = functionSchema;
+astNodeSchemas[ts.SyntaxKind.FunctionDeclaration] = functionSchema;
+astNodeSchemas[ts.SyntaxKind.FunctionExpression] = functionSchema;
+astNodeSchemas[ts.SyntaxKind.GetAccessor] = functionSchema;
+astNodeSchemas[ts.SyntaxKind.MethodDeclaration] = functionSchema;
+astNodeSchemas[ts.SyntaxKind.SetAccessor] = functionSchema;
+astNodeSchemas[ts.SyntaxKind.ConstructorType] = functionSchema;
+astNodeSchemas[ts.SyntaxKind.FunctionType] = functionSchema;
+astNodeSchemas[ts.SyntaxKind.IndexSignature] = functionSchema;
+
 // Prepend the base schema to every AST node-specific schema
 for (let i = 0; i < astNodeSchemas.length; ++i) {
   let schema = astNodeSchemas[i];
